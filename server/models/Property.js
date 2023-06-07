7b079a16424dbc1b4c16b959230d810b02fa22fb
ReshadSadik <reshadsadik@gmail.com>
2023-06-07 11:45:11 +0600

    validation for property model

--- server/models/Property.js
+++ server/models/Property.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
 const PropertySchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  propertyType: { type: String, required: true },
+  title: {
+    type: String,
+    required: [true, "please provide a name for the property"],
+  },
+  description: {
+    type: String,
+    required: [true, "please provide a description for the property"],
+  },
+  propertyType: {
+    type: String,
+    required: [true, "please select a type for the property"],
+  },
   location: { type: String, required: true },
   price: { type: Number, required: true },
   photo: { type: String, required: true },
