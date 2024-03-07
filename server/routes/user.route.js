f87cd708f974597b4fbb9cdd0bd790c779f78374
ReshadSadik <reshadsadik@gmail.com>
2024-03-07 14:01:24 +0600

    added comment for user id

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const userController = require("../controllers/user.controller");
-const verifyUserToken = require("../middlewares/verifyRequest");
+const express = require('express');
+const userController = require('../controllers/user.controller');
+const verifyUserToken = require('../middlewares/verifyRequest');
 
 const router = express.Router();
 
 router
-  .route("/")
+  .route('/')
   .get(verifyUserToken, userController.getAllAgents)
   .post(userController.createUser);
-router.post("/signup", userController.signUp);
-router.post("/login", userController.login);
-router.route("/:id").get(userController.getAgentByID);
+router.post('/signup', userController.signUp);
+router.post('/login', userController.login);
+router.route('/:id').get(userController.getAgentByID); // get user by a specific id
 
 module.exports = router;
